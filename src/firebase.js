a2bc50b01d226168f19796968828c7f002a4ce0b
evanlv <evanlvdev@gmail.com>
2021-10-01 12:24:53 +0200

    feat: initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/base";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDZAHq2tlYX9q8qmCck8ob0bR9ZLpw25wA",
+  authDomain: "auth-react-5c003.firebaseapp.com",
+  projectId: "auth-react-5c003",
+  storageBucket: "auth-react-5c003.appspot.com",
+  messagingSenderId: "1083893571319",
+  appId: "1:1083893571319:web:13ead42661dbaffdd17df2",
+});
+export default app;
+
+export const auth = app.auth();
