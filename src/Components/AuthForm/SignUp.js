81e737c813d65c10591e1e240ebf6a4b2a290594
evanlv <evanlvdev@gmail.com>
2021-10-01 13:46:08 +0200

    feat: signup

--- src/Components/AuthForm/SignUp.js
+++ src/Components/AuthForm/SignUp.js
@@ -1,31 +1,70 @@
+import { useContext, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+// import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 import "./AuthForm.css";
 
 const SignUp = () => {
+  const [error, setError] = useState("");
+  // const history = useHistory;
+  const { signup } = useContext(AuthContext);
+
   const showModal = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const closeModal = () => {
     dispatch({ type: "CLOSEMODAL" });
   };
+
+  const inputs = useRef([]);
+
+  const addInputs = (el) => {
+    if (el && !inputs.current.includes(el)) {
+      inputs.current.push(el);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (inputs.current[1].value !== inputs.current[2].value) {
+      setError("Passwords are not the same");
+      return;
+    }
+    await signup(inputs.current[0].value, inputs.current[2].value);
+    // history.push("/loggedHome");
+    dispatch({ type: "CLOSEMODAL" });
+
+    inputs.current.forEach((input) => {
+      input.value = "";
+    });
+    setError("");
+  };
+
   return (
     <div className={showModal.showSignUp ? "global-modal" : "hide-modal"}>
       <div className="overlay" onClick={closeModal}></div>
       <div className="container-modal">
-        <form className="form-auth">
+        <form className="form-auth" onSubmit={handleSubmit}>
           <h2>Registration</h2>
 
           <label htmlFor="mail">Email</label>
-          <input type="email" htmlFor="mail" required />
+          <input type="email" htmlFor="mail" required ref={addInputs} />
 
           <label htmlFor="password">Password</label>
-          <input type="password" htmlFor="password" required />
+          <input type="password" htmlFor="password" required ref={addInputs} />
 
           <label htmlFor="confirm-password">Confirm password</label>
-          <input type="password" htmlFor="confirm-password" required />
+          <input
+            type="password"
+            htmlFor="confirm-password"
+            required
+            ref={addInputs}
+          />
 
-          <button className="btn-sign">Connect</button>
+          {error}
+
+          <button className="btn-sign">Register</button>
         </form>
         <button className="btn-close" onClick={closeModal}>
           X
